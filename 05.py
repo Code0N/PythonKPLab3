455ac6c812733716e9ea81d8728ec2e22463ed26
Code0N <redeemer.of.souls@yandex.ru>
2017-04-29 17:04:49 +0300

    Реализован 1 и 2 методы класса

--- 05.py
+++ 05.py
@@ -1,3 +1,16 @@
 class StringFormatter:
-	def delNLines(self, string):
+	def delNLines(self, string, n):
+		wordslist = string.split(' ')
+		for i in wordslist:
+			if len(i) < n:
+				wordslist.remove(i)
+		return ' '.join(wordslist)
 		
+	def replaceNumbers(self, string):
+		tempstr = string.translate(str.maketrans('0123456789', '**********'))
+		return tempstr
+
+
+SF = StringFormatter()
+print(SF.delNLines('123 123456 1234 1234567 12345', 6))
+print(SF.replaceNumbers('Кошка мыла 4 лапы и 1 морду'))

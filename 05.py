3101facdfa3b7b5e7e00d0516e9073c567fcc887
Code0N <redeemer.of.souls@yandex.ru>
2017-04-29 17:38:50 +0300

    Реализованы 4 метода из 5-ти

--- 05.py
+++ 05.py
@@ -9,8 +9,29 @@ class StringFormatter:
 	def replaceNumbers(self, string):
 		tempstr = string.translate(str.maketrans('0123456789', '**********'))
 		return tempstr
+	
+	def addSpaces(self, string):
+		#Здравствуйте, здравствуйте дорогие друзья
+		#В эфире радио "Щиткод FM" и наша постоянная передача
+		#Костыль FM. И сегодня мы с вами будем бегать циклом по строке
+		#Зато работает
+		tempstr = ''
+		for i in string:
+			if not i == ' ':
+				tempstr += i + ' '
+		return tempstr
+		
+	def sortByLenght(self, string):
+		def SBL(str):
+			return len(str)
+		templst = string.split(' ')
+		templst.sort(key=SBL)
+		return ' '.join(templst)
+		
 
 
 SF = StringFormatter()
-print(SF.delNLines('123 123456 1234 1234567 12345', 6))
+print(SF.delNLines('123 123456 1234 1234567 12345', 5))
 print(SF.replaceNumbers('Кошка мыла 4 лапы и 1 морду'))
+print(SF.addSpaces('Hello, mister Anderson'))
+print(SF.sortByLenght('Привет, я Дед Лайн, и я пришёл, чтобы забрать у тебя твою бесполезную жизнь'))
